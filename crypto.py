ab6546ff4d725a461fba34f23c43c709381c6a20
ghost-mann <iamaob8@protonmail.com>
2025-08-11 18:16:45 +0300

    feat: updated code to fetch all crypto prices pairs in crypto.py

--- crypto.py
+++ crypto.py
@@ -1,8 +1,16 @@
-import pandas
+import pandas as pd
 import requests
 
 base_url = 'https://api.binance.com'
 
 def get_latest_prices():
     url = f'{base_url}/api/v3/ticker/price'
+    response = requests.get(url)
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df)
+    return df
+
+if __name__ == "__main__":
+    get_latest_prices()
     
