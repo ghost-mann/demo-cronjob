dfea3d0a1e49ba10c92a1c0ca7d7bf2dbabd5730
ghost-mann <iamaob8@protonmail.com>
2025-08-11 18:42:26 +0300

    feat: added filtered_df to crypto.py

--- crypto.py
+++ crypto.py
@@ -2,14 +2,16 @@ import pandas as pd
 import requests
 
 base_url = 'https://api.binance.com'
+TOPPAIRS = ['BTCUSDT','ETHBTC','ETHUSDT','SOLUSDT']
 
 def get_latest_prices():
     url = f'{base_url}/api/v3/ticker/price'
     response = requests.get(url)
     data = response.json()
     df = pd.DataFrame(data)
-    print(df)
-    return df
+    filtered_df = df[df['symbol'].isin(TOPPAIRS)]
+    print(filtered_df)
+    return filtered_df
 
 if __name__ == "__main__":
     get_latest_prices()
